8c127991345b0956b2e4f3d70efcf3363e6bc948
Mendel E <rastamanjohn10@gmail.com>
2019-01-02 19:03:44 -0500

    add some branding xD
    Change-Id: I0915cfd303b0024d77a256c96f58a821d400b989

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -827,6 +827,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("#   \ \_____\ \_\, \_\ \_\ \_\/\_____\#");
   script.Print("#    \/_____/\/_/ \/_/\/_/\/_/\/_____/#");
   script.Print("#                                     #");
+  script.Print("#           by rastamanjohn           #");
+  script.Print("#                                     #");
   script.Print("#######################################");
 
   if OPTIONS.backuptool:
